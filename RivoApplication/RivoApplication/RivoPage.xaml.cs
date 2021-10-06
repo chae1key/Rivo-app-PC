3cd41cba965baf3ff826e64ea8c0c95cce0793d2
KaeHyun <80453200+KaeHyun@users.noreply.github.com>
2021-10-06 22:20:17 +0900

    리보 어플리케이션 windows 부분 커밋

---
+++ RivoApplication/RivoApplication/RivoPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+
+namespace RivoApplication
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class RivoPage : Page
+    {
+        private DeviceWatcher deviceWatccher;
+        public RivoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SearchStatus.IsActive = true;
+            StartBLEDeviceWatcher();
+        }
+
+        private void StartBLEDeviceWatcher()
+        {
+            //deviceWatccher = new BluetoothLEAdvertisementWatcher
+            //{
+            //    ScanningMode = BluetoothLEScanningMode.Active
+            //};
+
+        }
+    }
+}
